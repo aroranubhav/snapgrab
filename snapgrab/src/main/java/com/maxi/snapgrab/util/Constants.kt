1388cf740cfc2a48d3ebb6537c7dd0c390acdcda
maxi <arora.anubhav93@gmail.com>
2025-07-01 22:42:38 +0530

    Add constant values

---
+++ snapgrab/src/main/java/com/maxi/snapgrab/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.maxi.snapgrab.util
+
+object Constants {
+    const val DEFAULT_READ_TIMEOUT_MILLS = 20000
+    const val DEFAULT_CONNECTION_TIMEOUT_MILLS = 20000
+    const val RANGE = "Range"
+}
