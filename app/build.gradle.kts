2df215f375ce2dfc0ec989fd1c5f7650aabb5ed0
maxi <arora.anubhav93@gmail.com>
2026-01-27 13:56:22 +0530

    Imports the library, enables view binding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.maxi.filedownloader"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.maxi.filedownloader"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -33,9 +33,13 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
+    implementation(project(":snapgrab"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
