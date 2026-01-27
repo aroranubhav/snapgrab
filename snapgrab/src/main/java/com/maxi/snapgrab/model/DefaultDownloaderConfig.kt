ab588ba0e10c88bbc0c3447888482e0460faf671
maxi <arora.anubhav93@gmail.com>
2026-01-27 13:33:13 +0530

    Add model classes

--- snapgrab/src/main/java/com/maxi/snapgrab/model/DefaultDownloaderConfig.kt
+++ snapgrab/src/main/java/com/maxi/snapgrab/model/DefaultDownloaderConfig.kt
@@ -1,3 +1,12 @@
 package com.maxi.snapgrab.model
 
-data class DefaultDownloaderConfig()
+import com.maxi.snapgrab.httpclient.DefaultHttpClient
+import com.maxi.snapgrab.httpclient.HttpClient
+import com.maxi.snapgrab.utils.Constants.CONNECTION_TIME_OUT
+import com.maxi.snapgrab.utils.Constants.READ_TIME_OUT
+
+data class DefaultDownloaderConfig(
+    val client: HttpClient = DefaultHttpClient(),
+    val connectionTimeOut: Long = CONNECTION_TIME_OUT,
+    val readTimeOut: Long = READ_TIME_OUT
+)
--- snapgrab/src/main/java/com/maxi/snapgrab/model/HttpResponse.kt
+++ snapgrab/src/main/java/com/maxi/snapgrab/model/HttpResponse.kt
@@ -1,3 +1,11 @@
 package com.maxi.snapgrab.model
 
-data class HttpResponse()
+import java.io.InputStream
+
+data class HttpResponse(
+    val responseCode: Int,
+    val totalBytes: Long,
+    val contentLength: Long,
+    val inputStream: InputStream,
+    val supportsResume: Boolean
+)
